c28da3066f7cd606fd0964918c155dae7e61f78e
ricknieto <rnieto@ucgdl.edu.mx>
2024-06-08 10:25:54 -0600

    Clase4

---
+++ libros.js
@@ -0,0 +1,12 @@
+let libreria = {
+    libros:{
+        titulo: "La biblia",
+        autor: "Dios",
+        fechaImpresion: "Siglo 0",
+        Precio: "$400 mxn"
+
+    }
+
+}
+
+console.log(libreria);
---
+++ variables.js
@@ -0,0 +1,10 @@
+var numero = 3 
+console.log(numero)
+
+let texto = "Hola"
+
+console.log(texto)
+
+const PI = 3.1416
+console.log(PI)
+
