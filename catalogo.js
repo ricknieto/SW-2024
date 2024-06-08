3b090e4a06b283c116776ba09d0c0546f9710055
ricknieto <rnieto@ucgdl.edu.mx>
2024-06-08 13:32:29 -0600

    tarea js

---
+++ catalogo.js
@@ -0,0 +1,50 @@
+let catalogo = {
+    cd: [
+    {
+        titulo: "Thriller",
+        artista: "Michael Jackson",
+        compania: "Epic",
+        lanzamiento: "1982",
+        precio: "$150"
+
+    },
+    {
+        titulo: "The Dark Side of the Moon",
+        artista: "Pink Floyd",
+        compania: "Harvest",
+        lanzamiento: "1973",
+        precio: "$200"
+
+    }
+    ,
+    {
+        titulo: "Back in Black",
+        artista: "AC/DC",
+        compania: "Atlantic",
+        lanzamiento: "1980",
+        precio: "$180"
+
+    }
+    ,
+    {
+        titulo: "Abbey Road",
+        artista: "The Beatles",
+        compania: "Apple",
+        lanzamiento: "1969",
+        precio: "$170"
+
+    }
+    ,
+    {
+        titulo: "21",
+        artista: "Adele",
+        compania: "XL",
+        lanzamiento: "2011",
+        precio: "$140"
+
+    }
+]
+}
+
+
+console.log(catalogo.cd);   
---
+++ menu.js
@@ -0,0 +1,45 @@
+let menu = {
+    comida: [
+    {
+        nombre: "Hamburgesa Clasica",
+        precio: "$179",
+        descripcion: "Hamburguesa con queso, lechuga, tomate y salsa especial.",
+        calorias: "550"
+
+    },
+    {
+        nombre: "Ensalada César",
+        precio: "$150",
+        descripcion: "Ensalada fresca con aderezo César, pollo a la parrilla y crutones",
+        calorias: "300"
+
+    }
+    ,
+    {
+        nombre: "Tacos de Pescado",
+        precio: "$195",
+        descripcion: "Tacos con pescado a la parrilla, col, y salsa de aguacate.",
+        calorias: "400"
+
+    }
+    ,
+    {
+        nombre: "Pizza Margherita",
+        precio: "$210",
+        descripcion: "Pizza con salsa de tomate, mozzarella fresca y albahaca.",
+        calorias: "600"
+
+    }
+    ,
+    {
+        nombre: "Pasta Alfredo",
+        precio: "$240",
+        descripcion: "Pasta con salsa Alfredo, pollo y queso parmesano.",
+        calorias: "700"
+
+    }
+]
+}
+
+
+console.log(menu.comida);   
