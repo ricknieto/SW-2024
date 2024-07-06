a4ebe0aa43fb4a3a803837143ee55d4094e14084
ricknieto <rnieto@ucgdl.edu.mx>
2024-07-06 13:50:33 -0600

    creationmodel

---
+++ Biblioteca/models/libro.js
@@ -0,0 +1,13 @@
+class Libro{
+    titulo = "";
+    autor = "";
+    fecha = "";
+    codigo = "";
+
+    constructor(titulo, autor, fecha, codigo) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.fecha = fecha;
+        this.codigo = codigo;
+    }
+}
